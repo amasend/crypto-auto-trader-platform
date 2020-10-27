48deeec632bc7c09a8babeb38ba0b6e663bbb162
Franciszek Job <54181625+Fiiranek@users.noreply.github.com>
2020-10-27 14:54:57 +0100

    Update download_manager.py

--- downloader/download_manager.py
+++ downloader/download_manager.py
@@ -3,7 +3,9 @@ from downloader.history_saver.save_manager import HistorySaver
 
 bitcon_symbol = 'BTC/USDT'
 exchange_name = 'binance'
-bitcon_history = HistoryDownloader(exchange_name=exchange_name, crypto_symbol=bitcon_symbol).get_history()
+binance_bitcoin = HistoryDownloader(exchange_name=exchange_name, crypto_symbol=bitcon_symbol)
+
+bitcoin_history = binance_bitcoin.getHistory()
 
 # save bitcoin history to csv file
 HistorySaver(bitcon_history).save_history_to_csv("test")
