a9aa2a2f362edb54434556cfedc6964ffa61c637
Franciszek Job <54181625+Fiiranek@users.noreply.github.com>
2020-10-27 13:24:08 +0100

    Added save_manager

---
+++ downloader/history_saver/save_manager.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+
+class HistorySaver:
+    def __init__(self, crypto_history: dict):
+        self.crypto_history = crypto_history
+
+    def save_history_to_csv(self, csv_name: str):
+        """ saving cryptocurrency history to csv file
+            Paramaters
+            ------------
+            csv_name:str required
+        """
+        crypto_history_csv = pd.DataFrame(self.crypto_history)
+        crypto_history_csv.to_csv(f'./crypto_history_csv/{csv_name}.csv', index=False)
